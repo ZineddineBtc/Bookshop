e5d5d2be089c6b371b5b6c4dd03e77164928ded9
Zineddine Bettouche <zineddine.bettouche@stud.th-deg.de>
2020-11-17 16:00:38 +0100

    LogoActivity implemented

---
+++ app/src/main/java/com/example/bookshop/LogoActivity.java
@@ -0,0 +1,26 @@
+package com.example.bookshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Objects;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), SliderActivity.class));
+            }
+        }, 1000);
+    }
+}
--- app/src/main/java/com/example/bookshop/SliderActivity.java
+++ app/src/main/java/com/example/bookshop/SliderActivity.java
@@ -1,14 +1,15 @@
 package com.example.bookshop;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SliderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_slider);
+
+
     }
 }
